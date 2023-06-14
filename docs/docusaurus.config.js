d04f0e3bdafff622626a0e768669623a36c5405c
Bowen Xue <xboxue@gmail.com>
2023-06-13 23:38:20 -0700

    Fix docs preview

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
+  title: "Trench",
+  tagline: "Open-source payment fraud prevention platform",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
@@ -18,8 +18,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "trytrench", // Usually your GitHub org/user name.
+  projectName: "trench", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
