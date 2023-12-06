662713d862f74396af7c17f0fde4e5731a777531
Michael Wu <68759331+micwu562@users.noreply.github.com>
2023-12-05 17:08:50 -0800

    Make Navbar take up single row

--- dashboard/src/components/Navbar.tsx
+++ dashboard/src/components/Navbar.tsx
@@ -60,72 +60,69 @@ export const Navbar = () => {
   const activeTab = router.pathname.split("/")[1];
 
   return (
-    <nav
-      className={clsx({
-        "w-full self-center shrink-0 flex flex-col justify-start p-0": true,
-      })}
-    >
-      <div className="flex mt-2 px-4 items-center justify-start gap-4">
-        {!isDesktop && (
-          // missing onclick
-          <button className="-m-3 p-3" ref={btnRef}>
-            <Menu height={24} width={24} />
-          </button>
-        )}
-        <NextLink href="/">
-          <h1 className="text-lg font-bold text-emphasis-foreground mr-12">
-            Trench
-          </h1>
-        </NextLink>
+    <nav className="flex mt-2 px-4 items-center justify-start gap-4 border-b py-2">
+      {!isDesktop && (
+        // missing onclick
+        <button className="-m-3 p-3" ref={btnRef}>
+          <Menu height={24} width={24} />
+        </button>
+      )}
+      <NextLink href="/">
+        <h1 className="text-lg font-bold text-emphasis-foreground mx-4">
+          Trench
+        </h1>
+      </NextLink>
 
-        <div className="grow" />
-
-        <div className="flex items-center gap-4 text-sm">
-          <ThemeToggle />
-          <NavItem href="/changelog">Changelog</NavItem>
-          <NavItem href="/help">Help</NavItem>
-          <NavItem href="/docs">Docs</NavItem>
-          <DropdownMenu>
-            <DropdownMenuTrigger>
-              <Avatar className="w-9 h-9">
-                {/* <AvatarImage src="https://github.com/shadcn.png" /> */}
-                <AvatarFallback>
-                  {getInitials(session.data?.user.name ?? "")}
-                </AvatarFallback>
-              </Avatar>
-            </DropdownMenuTrigger>
-
-            <DropdownMenuContent>
-              <DropdownMenuItem
-                onClick={() => {
-                  void signOut();
-                  void router.push("/login");
-                }}
+      <Tabs
+        className=""
+        value={activeTab}
+        onValueChange={(tab) => {
+          router.push(`/${tab}`).catch(handleError);
+        }}
+      >
+        <TabsList className="pl-2 border-b-0">
+          {TABS.map((tab) => {
+            return (
+              <TabsTrigger
+                key={tab.path}
+                value={tab.path}
+                className="border-b-0 text-muted-foreground data-[state=active]:text-emphasis-foreground hover:text-emphasis-foreground"
               >
-                Log Out
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </div>
-      </div>
+                {tab.name}
+              </TabsTrigger>
+            );
+          })}
+        </TabsList>
+      </Tabs>
 
-      <div>
-        <Tabs
-          value={activeTab}
-          onValueChange={(tab) => {
-            router.push(`/${tab}`).catch(handleError);
-          }}
-        >
-          <TabsList className="pl-2">
-            {TABS.map((tab) => {
-              return (
-                <TabsTrigger key={tab.path} value={tab.path}>
-                  {tab.name}
-                </TabsTrigger>
-              );
-            })}
-          </TabsList>
-        </Tabs>
+      <div className="grow" />
+
+      <div className="flex items-center gap-4 text-sm">
+        <ThemeToggle />
+        <NavItem href="/changelog">Changelog</NavItem>
+        <NavItem href="/help">Help</NavItem>
+        <NavItem href="/docs">Docs</NavItem>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Avatar className="w-9 h-9">
+              {/* <AvatarImage src="https://github.com/shadcn.png" /> */}
+              <AvatarFallback>
+                {getInitials(session.data?.user.name ?? "")}
+              </AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent>
+            <DropdownMenuItem
+              onClick={() => {
+                void signOut();
+                void router.push("/login");
+              }}
+            >
+              Log Out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
 
       {/* <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
