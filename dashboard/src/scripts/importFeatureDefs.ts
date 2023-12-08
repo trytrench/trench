4dc64ad33cecb8be89d7d0b7dc6c129bee5d496e
Bowen Xue <xboxue@gmail.com>
2023-12-08 11:18:57 -0800

    Update import script

--- dashboard/src/scripts/importFeatureDefs.ts
+++ dashboard/src/scripts/importFeatureDefs.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Prisma } from "@prisma/client";
+import { PrismaClient, GlobalStateKey } from "@prisma/client";
 import data from "./featureDefs.json";
 
 const prisma = new PrismaClient({
@@ -9,30 +9,70 @@ const prisma = new PrismaClient({
 const featureDefs = data as any;
 
 async function main() {
-  featureDefs.forEach(async (fd: any) => {
-    // upsert
-    const snapshots = fd.snapshots.map((snapshot: any) => ({
-      eventTypes: snapshot.eventTypes,
-      deps: snapshot.deps,
-      config: snapshot.config,
-      createdAt: snapshot.createdAt,
-    }));
+  await Promise.all(
+    featureDefs.map(async (fd: any) => {
+      // upsert
+      const snapshots = fd.snapshots.map((snapshot: any) => ({
+        id: snapshot.id,
+        eventTypes: snapshot.eventTypes,
+        deps: snapshot.deps,
+        config: snapshot.config,
+        createdAt: snapshot.createdAt,
+      }));
 
-    const res = await prisma.featureDef.create({
-      data: {
-        type: fd.type,
-        name: fd.name,
-        dataType: fd.dataType,
-        createdAt: fd.createdAt,
-        updatedAt: fd.updatedAt,
+      const res = await prisma.featureDef.create({
+        data: {
+          id: fd.id,
+          type: fd.type,
+          name: fd.name,
+          dataType: fd.dataType,
+          createdAt: fd.createdAt,
+          updatedAt: fd.updatedAt,
 
-        snapshots: {
-          create: snapshots,
+          snapshots: {
+            create: snapshots,
+          },
+        },
+      });
+
+      console.log(`Added featureDef: ${res.name}`);
+    })
+  );
+
+  const latestFeatureSnapshots = await prisma.featureDef.findMany({
+    include: {
+      snapshots: {
+        orderBy: {
+          createdAt: "desc",
+        },
+        take: 1,
+      },
+    },
+  });
+
+  const engine = await prisma.executionEngine.create({
+    data: {
+      featureDefSnapshots: {
+        createMany: {
+          data: latestFeatureSnapshots.map((featureDef) => ({
+            featureDefSnapshotId: featureDef.snapshots[0]!.id,
+          })),
         },
       },
-    });
+    },
+  });
 
-    console.log(`Added featureDef: ${res.name}`);
+  await prisma.globalState.upsert({
+    where: {
+      key: GlobalStateKey.ActiveEngineId,
+    },
+    create: {
+      key: GlobalStateKey.ActiveEngineId,
+      value: engine.id,
+    },
+    update: {
+      value: engine.id,
+    },
   });
 }
 
