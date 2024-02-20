81712563d862933167a97aad8f3544d29bccce66
Bowen Xue <xboxue@gmail.com>
2024-02-19 21:27:54 -0800

    Fix imports for build

--- packages/event-processing/src/server/engine.ts
+++ packages/event-processing/src/server/engine.ts
@@ -1,5 +1,5 @@
 import PQueue from "p-queue";
-import { createRedisService } from "databases/src/redis";
+import { createRedisService } from "databases";
 import { assert } from "common";
 import {
   BASE_CONTEXT,
