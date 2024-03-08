6afbe4e8aa3d2ac692f4d2c7dc328db074416783
Bowen Xue <xboxue@gmail.com>
2024-03-08 14:24:26 -0800

    Update seed script

--- packages/databases/prisma/seed.ts
+++ packages/databases/prisma/seed.ts
@@ -1,19 +1,21 @@
-import { readdir, readFile } from "fs/promises";
-import path from "path";
 import { GlobalStateKey, prisma } from "..";
 
 async function main() {}
 
 main()
   .then(async () => {
-    const executionEngine = await prisma.executionEngine.create({});
+    const engines = await prisma.executionEngine.findMany();
 
-    await prisma.globalState.create({
-      data: {
-        key: GlobalStateKey.ActiveEngineId,
-        value: executionEngine.id,
-      },
-    });
+    if (!engines.length) {
+      const engine = await prisma.executionEngine.create({});
+
+      await prisma.globalState.create({
+        data: {
+          key: GlobalStateKey.ActiveEngineId,
+          value: engine.id,
+        },
+      });
+    }
 
     await prisma.$disconnect();
   })
